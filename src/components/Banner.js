160aded09c7736e6dbe5a726515c12fd5dbed5b1
donguramee <sanulim94@gmail.com>
2023-06-06 23:31:57 +0900

    객체 오류 해결

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "../api/axios";
+import requests from "../api/request";
 
 const Banner = () => {
+  useEffect(() => {
+    fetchData();
+    return () => {};
+  }, []);
+  const fetchData = () => {
+    const response = axios.get(requests.fetchNowPlaying);
+    console.log(response);
+  };
+
   return <div>Banner</div>;
 };
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 
 const Nav = () => {
+  const [show, setShow] = useState("false");
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow("true");
+      } else {
+        setShow("false");
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
   return (
-    <NavWrapper>
+    <NavWrapper show={show}>
       <Logo>
         <img
           alt="Disney Plus Logo"
@@ -22,7 +38,7 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${(props) => (props.show ? "#090b13" : "transparent")};
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -35,7 +51,7 @@ const Logo = styled.a`
   padding: 0;
   width: 80px;
   margin-top: 4px;
-  max-height: 0;
+  max-height: 70px;
   font-size: 0;
   display: inline-block;
 
