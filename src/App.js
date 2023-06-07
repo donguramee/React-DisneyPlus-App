5c1046f4f1f350bd1ab40d73d041bd72a6552dc9
donguramee <sanulim94@gmail.com>
2023-06-07 23:19:28 +0900

    배너 영상 추가 및 카테고리 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import "./App.css";
 import Banner from "./components/Banner";
+import Category from "./components/Category";
 import Nav from "./components/Nav";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Container>
       <Nav />
       <Banner />
+      <Category />
     </Container>
   );
 }
@@ -22,7 +24,7 @@ const Container = styled.main`
   top: 72px;
   padding: 0 calc(3.5vw + 5px);
 
-  &:after {
+  &::after {
     background: url("/images/home-background.png") center center / cover
       no-repeat fixed;
     content: "";
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { styled } from "styled-components";
 
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <Container>
+      <Wrapp>
+        <img src="/images/viewers-disney.png" alt="" />
+        <video autoPlay loop muted>
+          <source src="/videos/disney.mp4" type="video/mp4" />
+        </video>
+      </Wrapp>
+    </Container>
+  );
 };
 
 export default Category;
+
+const Container = styled.div`
+  margin-top: 30px;
+  padding-top: 30px 0px 26px;
+  display: grid;
+  gap: 25px;
+  grid-template-columns: repeat(5, 1fr);
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+const Wrapp = styled.div`
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0, 0, 0 / 69%) 0px 26px 30px -10px,
+    rgb(0, 0, 0 / 73%) 0px 16px 10px -10px;
+  curcor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgb(249, 249, 249, 0.1);
+`;
