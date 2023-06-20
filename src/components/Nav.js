4fe61078ea93ea4a72c660716044ce4ecfd3d0c3
donguramee <sanulim94@gmail.com>
2023-06-20 20:40:02 +0900

    react router dom 적용

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 
 const Nav = () => {
   const [show, setShow] = useState("false");
+  const { pathname } = useLocation();
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 50) {
-        setShow("true");
-      } else {
-        setShow("false");
-      }
-    });
-
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener("scroll", () => {});
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setShow("true");
+    } else {
+      setShow("false");
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
   return (
     <NavWrapper show={show}>
       <Logo>
@@ -27,11 +36,48 @@ const Nav = () => {
           onClick={() => (window.location.href = "/")}
         />
       </Logo>
+
+      {pathname === "/" ? (
+        <Login>Login</Login>
+      ) : (
+        <Input
+          value={searchValue}
+          onChange={handleChange}
+          className="nav__input"
+          type="text"
+          placeholder="검색해주세요."
+        />
+      )}
     </NavWrapper>
   );
 };
 
 export default Nav;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transperent;
+  }
+`;
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.582);
+  color: white;
+  padding: 5px;
+  border: none;
+`;
+
 const NavWrapper = styled.nav`
   position: fixed;
   top: 0;
