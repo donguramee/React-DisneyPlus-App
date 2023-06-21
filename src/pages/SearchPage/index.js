9a9419d85903c02fdb0b154c940835cd6734f2b1
donguramee <sanulim94@gmail.com>
2023-06-21 23:36:55 +0900

    searchPage

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
 
 const SearchPage = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const response = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(response.data.result);
+      console.log("response".response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>SearchPage</div>;
 };
 
